48d70fd894a68e4ea3bb8501051daef9686a007a
Maksim Dmitriev <amferot1007@gmail.com>
2026-09-21 16:13:38 +0300

    tests use page objects only

--- tests/test_ui_login.py
+++ tests/test_ui_login.py
@@ -2,8 +2,9 @@ from pages.login_page import LoginPage
 
 def test_login_success(page):
     products = LoginPage(page).open().login("standard_user", "secret_sauce")
-    assert page.locator("[data-test='title']").inner_text() == "Products"
+    assert products.get_title_text() == "Products"
 
 def test_login_wrong_password(page):
-    products = LoginPage(page).open().login("standard_user", "wrong_password")
-    assert page.locator(".error-message-container").is_visible()
+    login_page = LoginPage(page).open()
+    login_page.login("standard_user", "wrong_password")
+    assert login_page.is_error_visible()
